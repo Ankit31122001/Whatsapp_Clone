001b3587b2045f4e76c942e0d07d45c31abafb31
Ankit2001312 <ankit31.mishra2001@gmail.com>
2023-12-21 18:48:15 +0530

    Today

--- Whatsapp_backend/index.js
+++ Whatsapp_backend/index.js
@@ -8,22 +8,35 @@ const server = http.createServer(app);
 const io = socketIO(server);
 const {router}=require('./router/myrouter')
 app.use(express.static('Whatsapp_frontend'));
-//app.use(router)
+app.use(router)
 var names={}
+var ids ={}
 io.on('connection', (socket) => {
     console.log('A user connected');
     socket.on('user connected', (name) => {
         console.log('message: ' + name+socket.id);
          names[socket.id]=name
+         ids[name]=socket.id
        // console.log(names[socket.id])
     });
     socket.on('chat message', (msg) => { 
-        io.emit('chat message', { name: names[socket.id], message: msg });
+        let from=names[socket.id];
+        if(from==='A')
+        {io.to(ids['B']).emit("chat message",{name: names[socket.id],message:msg})}
+
+        else if (from==='B')
+        {
+            io.to(ids['C']).emit("chat message",{name: names[socket.id],message:msg})
+        }
+
+        else{
+        io.to(ids['A']).emit('chat message', { name: names[socket.id], message: msg });
+        }
     });
-    /* Handle disconnection
+     //Handle disconnection
     socket.on('disconnect', () => {
         console.log('User disconnected');
-    });*/
+    });
 });
 
 const PORT = 3000;
